ca2ec49d635ec917a34adfac10e3e1b43e2a7d77
Diego <diego.cedrim@gmail.com>
2018-04-27 16:13:07 -0300

    pagina de bem vindo

--- refresh_experiment/urls.py
+++ refresh_experiment/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/recommendations/', permanent=False), name='home'),
+    url(r'^$', RedirectView.as_view(url='/recommendations/welcome', permanent=False), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('recommendations/', include('recommendations.urls')),
